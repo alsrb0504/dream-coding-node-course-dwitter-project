70b4025e15bf6a016bf6510805b47306e19ea7a7
MinKyu Tae <alsrb8442@naver.com>
2022-08-03 17:19:45 +0900

    🎨 Practice client auth connection

--- client/src/service/auth.js
+++ client/src/service/auth.js
@@ -1,26 +1,58 @@
 export default class AuthService {
-  async login(username, password) {
+  constructor(http) {
+    this.http = http;
+  }
+
+  async signup(username, password, name, email, url) {
+    const res = await this.http.fetch(`/auth/signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        name,
+        email,
+        url,
+      }),
+    });
+
+    localStorage.setItem("token", res.token);
+
     return {
-      username: 'ellie',
-      token: 'abc1234',
+      username: res.username,
+      token: "abc1234",
+    };
+  }
+
+  async login(username, password) {
+    const res = await this.http.fetch(`/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
+
+    // 헤더에 토큰 설정..
+    localStorage.setItem("token", res.token);
+
+    return {
+      username: res.username,
+      // token: res.token,
     };
   }
 
   async me() {
+    const res = await this.http.fetch(`/auth/me`, {
+      method: "GET",
+    });
+
     return {
-      username: 'ellie',
-      token: 'abc1234',
+      username: res.username,
+      // token: "abc1234",
     };
   }
 
   async logout() {
-    return;
-  }
+    // localStorage.setItem("token", res.token);
+    localStorage.clear();
 
-  async signup(username, password, name, email, url) {
-    return {
-      username: 'ellie',
-      token: 'abc1234',
-    };
+    return;
   }
 }
